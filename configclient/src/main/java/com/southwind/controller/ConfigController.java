33b01d5ee3f7e77ffb8862c19347936e38e6554e
fan <fan@gmail.com>
2020-04-20 13:24:23 +0800

    no

---
+++ configclient/src/main/java/com/southwind/controller/ConfigController.java
@@ -0,0 +1,18 @@
+package com.southwind.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/config")
+public class ConfigController {
+    @Value("${server.port}")
+    private String port;
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index() {
+        return this.port;
+    }
+}

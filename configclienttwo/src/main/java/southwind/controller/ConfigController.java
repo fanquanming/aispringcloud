e5b3bce36c32f3b2e04387c1f2ff5fafbd2946ae
fan <fan@gmail.com>
2020-05-04 13:56:55 +0800

    i know

--- configclienttwo/src/main/java/southwind/controller/ConfigController.java
+++ configclienttwo/src/main/java/southwind/controller/ConfigController.java
@@ -1,7 +1,6 @@
-package com.southwind.controller;
+package southwind.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
